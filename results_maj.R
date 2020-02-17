83593b5a4915925cc65415ab8333023c1e9d051e
Okssana <40266071+Okssana@users.noreply.github.com>
2020-02-17 13:33:18 +0200

    Update and rename results_maj to results_maj.R

--- results_maj.R
+++ results_maj.R
@@ -3,7 +3,8 @@ library(dplyr)
 library(rvest)
 library(xml2)
 
-# Get majoritarian districts results
+# Функція для завантажування результатів виборів з сайту Центральної виборчої комісії ####
+# Get majoritarian districts results ####
 
 get_data <- function(){
   districts <- c(11:40, 45:52, 57:60, 62:103, 105:107, 112:223) # Available and non-occupied districts in Ukraine in July 2019
@@ -59,10 +60,13 @@ get_data <- function(){
   }
 }
 
+# Запуск завантаження ####
 major_mps <- get_data()
 
+# Читати файл ####
 majoritarian_elections <- read.csv2("majoritarian_elections_results.csv", header=FALSE)
 
+# Чистимо таблицю ####
 elections_res <- majoritarian_elections%>%
   mutate(V4=gsub(" ", "", majoritarian_elections$V4, fixed = TRUE))%>% # Get rid of white spaces
   mutate(V4=as.numeric(majoritarian_elections2$V4))
